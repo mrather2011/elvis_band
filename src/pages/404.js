4fff57d3179545db853df8f5ca47faef89a0ce50
mrather2011 <maxprop26@gmail.com>
2020-10-09 13:13:51 +0300

    fix to 404 path

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
 
-const NotFound = () => {
+const NotFound = props => {
   return (
     <Layout>
       <p>
