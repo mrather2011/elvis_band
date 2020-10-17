4692ef0d0ad79dc7699b065be511d534f460229e
mrather2011 <maxprop26@gmail.com>
2020-10-17 12:22:31 +0300

    enabling contact form

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import classes from "./Book.module.scss"
 import { useInView } from "react-intersection-observer"
 import { motion, useAnimation } from "framer-motion"
+import NumberFormat from "react-number-format"
 
 const Book = props => {
   const controls = useAnimation()
@@ -13,6 +14,70 @@ const Book = props => {
     }
   }, [controls, inView])
 
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    hearAboutUs: "",
+    other: "",
+  })
+
+  const formDataHandler = (e, item) => {
+    e.preventDefault()
+    switch (item) {
+      case "firstName":
+        setFormData({
+          ...formData,
+          firstName: e.target.value,
+        })
+        break
+      case "lastName":
+        setFormData({
+          ...formData,
+          lastName: e.target.value,
+        })
+        break
+      case "email":
+        setFormData({
+          ...formData,
+          email: e.target.value,
+        })
+        break
+      case "phone":
+        setFormData({
+          ...formData,
+          phone: e.target.value,
+        })
+        break
+      case "hearAboutUs":
+        setFormData({
+          ...formData,
+          hearAboutUs: e.target.value,
+        })
+        break
+      case "other":
+        setFormData({
+          ...formData,
+          other: e.target.value,
+        })
+        break
+      default:
+        return null
+    }
+  }
+
+  const clearFormData = () => {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      hearAboutUs: "",
+      other: "",
+    })
+  }
+  console.log(formData)
   return (
     <div className={classes.Container}>
       <div className={classes.HeaderText}>
@@ -30,28 +95,48 @@ const Book = props => {
         transition={{ duration: 1 }}
         className={classes.FormContainer}
       >
-        <form>
+        <form method="POST" data-netlify="true" onSubmit={clearFormData}>
           <div>
             <input
+              value={formData.firstName}
+              onChange={e => formDataHandler(e, "firstName")}
               type="text"
               name="first-name"
               placeholder="First Name"
             ></input>
-            <input type="text" name="last-name" placeholder="Last Name"></input>
+            <input
+              value={formData.lastName}
+              onChange={e => formDataHandler(e, "lastName")}
+              type="text"
+              name="last-name"
+              placeholder="Last Name"
+            ></input>
           </div>
 
           <div>
             <input
+              value={formData.email}
+              onChange={e => formDataHandler(e, "email")}
               type="email"
               name="email"
               placeholder="Email Address"
             ></input>
-            <input type="phone" name="phone" placeholder="Phone Number"></input>
+            <NumberFormat
+              format="(###) ###-####"
+              value={formData.phone}
+              onChange={e => formDataHandler(e, "phone")}
+              type="phone"
+              name="phone"
+              placeholder="Phone Number"
+            ></NumberFormat>
           </div>
 
           <div>
             <label>How Did You Hear About Us</label>
-            <select>
+            <select
+              value={formData.hearAboutUs}
+              onChange={e => formDataHandler(e, "hearAboutUs")}
+            >
               <option value="google">google</option>
               <option value="facebook">facebook</option>
               <option value="referral">referral</option>
@@ -60,6 +145,8 @@ const Book = props => {
           </div>
           <div>
             <textarea
+              value={formData.other}
+              onChange={e => formDataHandler(e, "other")}
               maxLength="300"
               wrap="hard"
               name="otherDetails"
@@ -67,7 +154,7 @@ const Book = props => {
             ></textarea>
           </div>
           <div>
-            <input type="submit" value="submit"></input>
+            <input type="submit" value="Submit"></input>
           </div>
         </form>
         <div className={classes.FormAction}>
